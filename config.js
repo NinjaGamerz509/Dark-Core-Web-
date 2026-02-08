bc80674795c0a144ea7fab45f288769582c5c1e8
NinjaGamerz509 <srahemat943@gmail.com>
2026-02-08 18:20:47 +0530

    Update inviteLink for Discord bot configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const botConfig = {
-    inviteLink: "",
+    inviteLink: "https://discord.com/oauth2/authorize?client_id=1467193900920864798&permissions=8&integration_type=0&scope=bot",
     supportServer: "https://discord.gg/KgKxVpw54F",
     discordLink: "https://discord.gg/KgKxVpw54F"
 };
